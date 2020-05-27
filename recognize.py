cfa82cd6c80b6335211fc95f38d8724e001de31b
comevback <xuxiang5012@gmail.com>
2020-05-27 22:27:04 +0800

    reco

--- recognize.py
+++ recognize.py
@@ -1,6 +1,6 @@
 import  speech_recognition as sr
 r = sr.Recognizer()
-sou = sr.AudioFile('hello.wav')
+sou = sr.AudioFile('reco.wav')
 with sou as source:
 	audio = r.record(source)
-print(r.recognize_google(audio))
+r.recognize_google(audio,show_all=True)
